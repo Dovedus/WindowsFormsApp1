e6057665980b47ed408c053d2a33c21f42ad0cbc
admin <admin@ROOM210-07>
2023-04-13 11:44:38 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Auth.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Auth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.labelLog = new System.Windows.Forms.Label();
+            this.labelPass = new System.Windows.Forms.Label();
+            this.buttonEnt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(175, 86);
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(158, 20);
+            this.textBoxLog.TabIndex = 0;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(175, 156);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(158, 20);
+            this.textBoxPass.TabIndex = 1;
+            // 
+            // labelLog
+            // 
+            this.labelLog.AutoSize = true;
+            this.labelLog.Location = new System.Drawing.Point(231, 70);
+            this.labelLog.Name = "labelLog";
+            this.labelLog.Size = new System.Drawing.Size(38, 13);
+            this.labelLog.TabIndex = 2;
+            this.labelLog.Text = "Логин";
+            // 
+            // labelPass
+            // 
+            this.labelPass.AutoSize = true;
+            this.labelPass.Location = new System.Drawing.Point(231, 140);
+            this.labelPass.Name = "labelPass";
+            this.labelPass.Size = new System.Drawing.Size(45, 13);
+            this.labelPass.TabIndex = 3;
+            this.labelPass.Text = "Пароль";
+            // 
+            // buttonEnt
+            // 
+            this.buttonEnt.Location = new System.Drawing.Point(187, 212);
+            this.buttonEnt.Name = "buttonEnt";
+            this.buttonEnt.Size = new System.Drawing.Size(125, 38);
+            this.buttonEnt.TabIndex = 4;
+            this.buttonEnt.Text = "Войти";
+            this.buttonEnt.UseVisualStyleBackColor = true;
+            this.buttonEnt.Click += new System.EventHandler(this.buttonEnt_Click);
+            // 
+            // Auth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 373);
+            this.Controls.Add(this.buttonEnt);
+            this.Controls.Add(this.labelPass);
+            this.Controls.Add(this.labelLog);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxLog);
+            this.Name = "Auth";
+            this.Text = "Auth";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Label labelLog;
+        private System.Windows.Forms.Label labelPass;
+        private System.Windows.Forms.Button buttonEnt;
+    }
+}
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.table1BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.db01DataSet = new WindowsFormsApp1.db01DataSet();
+            this.table1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.table_1TableAdapter = new WindowsFormsApp1.db01DataSetTableAdapters.Table_1TableAdapter();
+            this.db07DataSet = new WindowsFormsApp1.db07DataSet();
+            this.db07DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.usersTableAdapter = new WindowsFormsApp1.db07DataSetTableAdapters.UsersTableAdapter();
+            this.userIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userSurnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userLoginDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userPasswordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userRoleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db01DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db07DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db07DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.userIDDataGridViewTextBoxColumn,
+            this.userNameDataGridViewTextBoxColumn,
+            this.userSurnameDataGridViewTextBoxColumn,
+            this.userLoginDataGridViewTextBoxColumn,
+            this.userPasswordDataGridViewTextBoxColumn,
+            this.userRoleDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.usersBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(643, 325);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // table1BindingSource1
+            // 
+            this.table1BindingSource1.DataMember = "Table_1";
+            this.table1BindingSource1.DataSource = this.db01DataSet;
+            // 
+            // db01DataSet
+            // 
+            this.db01DataSet.DataSetName = "db01DataSet";
+            this.db01DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // table1BindingSource
+            // 
+            this.table1BindingSource.DataMember = "Table_1";
+            this.table1BindingSource.DataSource = this.db01DataSet;
+            // 
+            // table_1TableAdapter
+            // 
+            this.table_1TableAdapter.ClearBeforeFill = true;
+            // 
+            // db07DataSet
+            // 
+            this.db07DataSet.DataSetName = "db07DataSet";
+            this.db07DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // db07DataSetBindingSource
+            // 
+            this.db07DataSetBindingSource.DataSource = this.db07DataSet;
+            this.db07DataSetBindingSource.Position = 0;
+            // 
+            // usersBindingSource
+            // 
+            this.usersBindingSource.DataMember = "Users";
+            this.usersBindingSource.DataSource = this.db07DataSetBindingSource;
+            // 
+            // usersTableAdapter
+            // 
+            this.usersTableAdapter.ClearBeforeFill = true;
+            // 
+            // userIDDataGridViewTextBoxColumn
+            // 
+            this.userIDDataGridViewTextBoxColumn.DataPropertyName = "UserID";
+            this.userIDDataGridViewTextBoxColumn.HeaderText = "UserID";
+            this.userIDDataGridViewTextBoxColumn.Name = "userIDDataGridViewTextBoxColumn";
+            // 
+            // userNameDataGridViewTextBoxColumn
+            // 
+            this.userNameDataGridViewTextBoxColumn.DataPropertyName = "UserName";
+            this.userNameDataGridViewTextBoxColumn.HeaderText = "UserName";
+            this.userNameDataGridViewTextBoxColumn.Name = "userNameDataGridViewTextBoxColumn";
+            // 
+            // userSurnameDataGridViewTextBoxColumn
+            // 
+            this.userSurnameDataGridViewTextBoxColumn.DataPropertyName = "UserSurname";
+            this.userSurnameDataGridViewTextBoxColumn.HeaderText = "UserSurname";
+            this.userSurnameDataGridViewTextBoxColumn.Name = "userSurnameDataGridViewTextBoxColumn";
+            // 
+            // userLoginDataGridViewTextBoxColumn
+            // 
+            this.userLoginDataGridViewTextBoxColumn.DataPropertyName = "UserLogin";
+            this.userLoginDataGridViewTextBoxColumn.HeaderText = "UserLogin";
+            this.userLoginDataGridViewTextBoxColumn.Name = "userLoginDataGridViewTextBoxColumn";
+            // 
+            // userPasswordDataGridViewTextBoxColumn
+            // 
+            this.userPasswordDataGridViewTextBoxColumn.DataPropertyName = "UserPassword";
+            this.userPasswordDataGridViewTextBoxColumn.HeaderText = "UserPassword";
+            this.userPasswordDataGridViewTextBoxColumn.Name = "userPasswordDataGridViewTextBoxColumn";
+            // 
+            // userRoleDataGridViewTextBoxColumn
+            // 
+            this.userRoleDataGridViewTextBoxColumn.DataPropertyName = "UserRole";
+            this.userRoleDataGridViewTextBoxColumn.HeaderText = "UserRole";
+            this.userRoleDataGridViewTextBoxColumn.Name = "userRoleDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db01DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db07DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db07DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private db01DataSet db01DataSet;
+        private System.Windows.Forms.BindingSource table1BindingSource;
+        private db01DataSetTableAdapters.Table_1TableAdapter table_1TableAdapter;
+        private System.Windows.Forms.BindingSource table1BindingSource1;
+        private System.Windows.Forms.BindingSource db07DataSetBindingSource;
+        private db07DataSet db07DataSet;
+        private System.Windows.Forms.BindingSource usersBindingSource;
+        private db07DataSetTableAdapters.UsersTableAdapter usersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userSurnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userLoginDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userPasswordDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userRoleDataGridViewTextBoxColumn;
+    }
+}
+
